e1e2c37341627393384b4961c71509a9534b17b4
Anonymous202401 <158303541+Anonymous202401@users.noreply.github.com>
2024-02-02 18:57:57 +0800

    Update main_eva.py

--- main_eva.py
+++ main_eva.py
@@ -3,18 +3,14 @@ import time
 import matplotlib
 matplotlib.use('Agg')
 from utils.options import args_parser
-from Evaluate_Pearson import Evaluate_Pearson
-from Evaluate_Pearson import Evaluate_PearsonTEST
+
 from Evaluate_Euclidean import Evaluate_Euclidean
 from Evaluate_Euclidean import Evaluate_EuclideanTEST
-from Evaluate_Scatter import Evaluate_Scatter
-from Evaluate_Scatter import Evaluate_ScatterTEST
+
 from Evaluate_CKA import Evaluate_CKA
 from Evaluate_CKA import Evaluate_CKATest
 
 
 args = args_parser()
-Evaluate_Pearson(args)
-Evaluate_Scatter(args)
 Evaluate_Euclidean(args)
 Evaluate_CKA(args)
