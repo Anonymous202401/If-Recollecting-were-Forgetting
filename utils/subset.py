82452fabc05328dc9baa50780be201674dc7afad
Anonymous202401 <158303541+Anonymous202401@users.noreply.github.com>
2024-05-23 04:31:09 +0800

    Update subset.py

--- utils/subset.py
+++ utils/subset.py
@@ -9,3 +9,10 @@ def reduce_dataset_size(dataset, max_samples,random_seed):
     subset_indices = torch.randperm(len(dataset))[:max_samples]
     reduced_dataset = Subset(dataset, subset_indices)
     return reduced_dataset
+
+def sample_dataset_size(dataset, random_seed,indices_to_unlearn):
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    reduced_dataset = Subset(dataset, indices_to_unlearn)
+    return reduced_dataset
