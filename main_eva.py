a7d79b1bff0dfabbaab939a2501b45125db7435f
Anonymous202401 <158303541+Anonymous202401@users.noreply.github.com>
2024-10-01 23:49:06 +0800

    Update main_eva.py

--- main_eva.py
+++ main_eva.py
@@ -4,10 +4,10 @@ import matplotlib
 matplotlib.use('Agg')
 from utils.options import args_parser
 from utils.Evaluate_Euclidean import Evaluate_Euclidean
-from utils.Evaluate_Euclidean import Evaluate_EuclideanTEST
+from utils.Evaluate_Euclidean import Evaluate_Euclidean_ResNet
 
 args = args_parser()
 if args.dataset in ['mnist', 'fashion-mnist']:
     Evaluate_Euclidean(args)
 else:
-    Evaluate_EuclideanTEST(args)
+    Evaluate_Euclidean_ResNet(args)
