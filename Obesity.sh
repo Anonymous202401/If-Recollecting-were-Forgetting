22ea1bc035ddc2e4d2d88751f475c819f6c2fa1a
Anonymous202401 <158303541+Anonymous202401@users.noreply.github.com>
2024-11-18 00:28:13 +0800

    Update Obesity.sh

--- Obesity.sh
+++ Obesity.sh
@@ -1,24 +1,6 @@
-# for algorithm in main_proposed.py main_retrain.py main_NU.py main_IJ.py main_eva.py; do
-# for seed in 42 124 3407 114514 5 1; do
-#     python3 -u $algorithm  --model mlp --dataset obesity --epochs 100 --num_dataset 2111 --batch_size 64 --num_forget 106 --lr 0.05 --regularization 0.001 --lr_decay 0.9995 --clip 10 --gpu 1  --seed $seed
-# done
-# done
-
-
-for algorithm in main_proposed.py ; do
+for algorithm in main_proposed.py main_retrain.py main_NU.py main_IJ.py main_eva.py; do
 for seed in 42 124 3407 114514 5 1; do
     python3 -u $algorithm  --model mlp --dataset obesity --epochs 100 --num_dataset 2111 --batch_size 64 --num_forget 106 --lr 0.05 --regularization 0.001 --lr_decay 0.9995 --clip 10 --gpu 1  --seed $seed
 done
 done
 
-for algorithm in main_retrain.py; do
-for seed in 42 124 3407 114514 5 1; do
-    python3 -u $algorithm  --model mlp --dataset obesity --epochs 100 --num_dataset 2111 --batch_size 64 --num_forget 106 --lr 0.05 --regularization 0.001 --lr_decay 0.9995 --clip 10 --gpu 2  --seed $seed
-done
-done
-
-for algorithm in main_NU.py main_IJ.py main_eva.py; do
-for seed in 42 124 3407 114514 5 1; do
-    python3 -u $algorithm  --model mlp --dataset obesity --epochs 100 --num_dataset 2111 --batch_size 64 --num_forget 106 --lr 0.05 --regularization 0.001 --lr_decay 0.9995 --clip 10 --gpu 3  --seed $seed
-done
-done
